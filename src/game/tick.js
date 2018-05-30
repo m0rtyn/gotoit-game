7637c860894fb76a5c3f61744a23be930793d4f2
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-05-30 11:34:13 +0300

    achievement notifications added

--- src/game/tick.js
+++ src/game/tick.js
@@ -11,7 +11,10 @@ export function tick(state) {
 
     _.each(achievements, (achievement, key) => {
         if (state.data.achieved[key]) return
-        if (achievement.rule(state)) state.data.achieved[key] = true
+        if (achievement.rule(state)) {
+            state.data.achieved[key] = true
+            state.data.helpers.addAction(`${achievement.name} ${achievement.rank} achievement unlocked!`, {timeOut: 3000, extendedTimeOut: 2000}, 'success')
+        }
     })
 
     return state;
