8e9c7c081582e15903dbdd4df31a6e9ce4bfaffa
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-05-31 15:04:29 +0300

    achievements iteration fixed

--- src/game/tick.js
+++ src/game/tick.js
@@ -10,7 +10,7 @@ export function tick(state) {
     });
 
     _.each(achievements, (achievement, key) => {
-        if (state.data.achieved[key]) return
+        if (state.data.achieved[key] === true) return
         if (achievement.rule(state)) {
             state.data.achieved[key] = true
             state.data.helpers.addAction(`${achievement.name} ${achievement.rank} achievement unlocked!`, {timeOut: 3000, extendedTimeOut: 2000}, 'success')
