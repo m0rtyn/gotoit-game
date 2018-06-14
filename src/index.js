575fdc6f8fbd55b59a5981374cf881ddc117b8d5
Volodya Martynson <zogacc@gmail.com>
2018-06-14 20:57:27 +0700

    add local bootstrap3 files

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import {IntlProvider} from 'react-intl';
 //window.jQuery = jQuery = window.$ = $;
 //console.log($);
 
+import './bootstrap/css/bootstrap.min.css'; //TODO: bug with black screen instead all modal windows //.modal-backdrop
+
+// theAdmin theme =============================
+// import './theme/src/assets/css/style.min.css';
+// import './theme/src/assets/css/core.min.css'; //TODO: bug with black screen instead all modal windows
+import './theme/src/assets/css/app.min.css'; //.dialog-backdrop
+// ============================================
+
 import App from './App';
 import './css/index.css';
 
-// theAdmin theme =============================
-import './theme/src/assets/css/style.min.css';
-import './theme/src/assets/css/core.min.css';
-import './theme/src/assets/css/app.min.css';
-// ============================================
 
 ReactDOM.render(
   <IntlProvider locale="en">
