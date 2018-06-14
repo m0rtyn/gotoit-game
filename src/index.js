eb624808354299b15299f397ec3cbdc972523811
Volodya Martynson <zogacc@gmail.com>
2018-06-14 20:58:55 +0700

    replace styles import in index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import {IntlProvider} from 'react-intl';
 //window.jQuery = jQuery = window.$ = $;
 //console.log($);
 
-import './bootstrap/css/bootstrap.min.css'; //TODO: bug with black screen instead all modal windows //.modal-backdrop
+// import './bootstrap/css/bootstrap.min.css'; //TODO: bug - не могу пока отключить, пропадает белый фон всех блоков интерфейса
 
 // theAdmin theme =============================
-// import './theme/src/assets/css/style.min.css';
-// import './theme/src/assets/css/core.min.css'; //TODO: bug with black screen instead all modal windows
-import './theme/src/assets/css/app.min.css'; //.dialog-backdrop
+import './theme/src/assets/css/style.min.css';
+import './theme/src/assets/css/core.min.css';
+import './theme/src/assets/css/app.min.css';
 // ============================================
+
+import './css/bootstrap-slider.css';
+import './css/App.css';
 
 import App from './App';
 import './css/index.css';
