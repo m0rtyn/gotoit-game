6bcd70a58beb7348fccdbac75fea855cb8f2a134
Volodya Martynson <zogacc@gmail.com>
2018-06-15 18:42:53 +0700

    replace libraries files in one folder

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import {IntlProvider} from 'react-intl';
 //window.jQuery = jQuery = window.$ = $;
 //console.log($);
 
-// import './bootstrap/css/bootstrap.min.css'; //TODO: bug - не могу пока отключить, пропадает белый фон всех блоков интерфейса
+// import './libraries/bootstrap/css/bootstrap.min.css'; //TODO: bug - не могу пока полностью отключить, ломается позиционирование попапов
 
 // theAdmin theme =============================
-import './theme/src/assets/css/style.min.css';
-import './theme/src/assets/css/core.min.css';
-import './theme/src/assets/css/app.min.css';
+import './libraries/theme/src/assets/css/style.min.css';
+import './libraries/theme/src/assets/css/core.min.css';
+import './libraries/theme/src/assets/css/app.min.css';
 // ============================================
 
-import './css/bootstrap-slider.css';
+import './libraries/toastr/toastr.css';
+import './libraries/animate/animate.css';
+import './libraries/bootstrap-slider.css';
+
 import './css/App.css';
 
 import App from './App';
