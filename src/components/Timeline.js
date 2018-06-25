2e1d43f7cd7d4778375921e65b4edad18601bbe2
Volodya Martynson <zogacc@gmail.com>
2018-06-25 12:33:30 +0700

    fix timeline width and make beautyfull

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,7 +5,7 @@ import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 // import '../assets/css/Timeline.css';
 import {project_kinds, project_platforms} from "../game/knowledge";
 
-const timelineWidth = 1280; //TODO: переделать, нужен гибкий метод определения ширины вьюпорта
+const timelineWidth = window.innerWidth;
 
 
 class Timeline extends Component {
@@ -19,11 +19,13 @@ class Timeline extends Component {
         return (
             <div className="gti-timeline-wrapper">
                 <div className='col-12 gti-timeline'>
+
                     <div className='line'>
-                        <div style={{ position: 'absolute', height: 10,
-                            width: 3, background: 'red', marginLeft: timelineWidth/2, zIndex: 10 }}>
-                            <p style={{ position: 'absolute', bottom: 1}}>Now</p>
-                        </div>
+                        <div
+                            className='now'
+                            style={{ marginLeft: timelineWidth / 2 }}
+                        ></div>
+
                         {
                             _.map(timelineScale, (day, index) => {
                                     let events = timelineEvents.filter( (item) => {
@@ -31,32 +33,42 @@ class Timeline extends Component {
                                             return true
                                         } else return false
                                     });
+
                                     return (
-                                        <div style={{
-                                            marginLeft: timelineWidth/(timelineScale.length-1) * index+ 'px'
-                                        }}>
-                                            <div style={{ position: 'absolute'}}>
+                                        <div 
+                                        className='step'
+                                        style={{ marginLeft: timelineWidth / (timelineScale.length - 1) * index + 'px' }}
+                                        >
+                                            
+                                            <div>
                                                 {day.getDate()}
                                             </div>
 
-                                            <div style={{ position: 'absolute', bottom: '10px', right: '10px'}}>
+                                            <div className="worker-portrait">
                                             {
                                                 _.map(events, (item) => {
                                                     if (item.type === 'deadline') {
                                                         return (
 
-                                                            <OverlayTrigger key={index} delay={150} placement="bottom"
-                                                                            overlay={
-                                                                                <Tooltip id={`Tooltip${index}`}>
-                                                                                    <div>
-                                                                                        {item.info + ': ' + item.object.name}
-                                                                                    </div>
-                                                                                </Tooltip>
-                                                                            }>
-                                                                <div style={{ position: 'relative', bottom: '18px' }}>
-                                                                    <div style={{border: '1px solid red', position: 'absolute'}}>
-                                                                        <img alt={item.object.name + ' platform'} src={require(`../../public/${project_platforms[item.object.platform].name}.svg`)}
-                                                                            width={20} height={20}/>
+                                                            <OverlayTrigger 
+                                                            key={index}
+                                                            delay={150}
+                                                            placement="bottom"
+                                                            overlay={
+                                                                <Tooltip id={`Tooltip${index}`}>
+                                                                    <div>
+                                                                        {item.info + ': ' + item.object.name}
+                                                                    </div>
+                                                                </Tooltip>
+                                                            }>
+                                                                <div style={{ position: 'relative' }}>
+                                                                    <div style={{ position: 'absolute' }}>
+                                                                        <img 
+                                                                        alt={item.object.name + ' platform'} 
+                                                                        src={require(`../../public/${project_platforms[item.object.platform].name}.svg`)}
+                                                                        width={20} 
+                                                                        height={20}
+                                                                        />
                                                                     </div>
                                                                     <div style={{ position: 'absolute' }}>
                                                                         <img alt={item.object.name + ' kind'} src={require(`../../public/${project_kinds[item.object.kind].name}.svg`)}
@@ -68,22 +80,22 @@ class Timeline extends Component {
                                                         )
                                                     } else if (item.type === 'vacation' || item.type === 'leave'){
                                                         return (
-                                                            <OverlayTrigger key={index} delay={150} placement="bottom"
-                                                                overlay={
-                                                                    <Tooltip id={`Tooltip${index}`}>
-                                                                        <div>
-                                                                            {item.info + ': ' + item.object.name}
-                                                                        </div>
-                                                                    </Tooltip>
-                                                                }
-                                                            >
-                                                                <img className='worker-portrait' style={{
-                                                                        border: '1px solid red',
-                                                                        width: 20,
-                                                                        height: 20
-                                                                    }}
-                                                                    src={item.object.avatar}
+                                                            <OverlayTrigger 
+                                                            key={index} 
+                                                            delay={150} 
+                                                            placement="bottom"
+                                                            overlay={
+                                                                <Tooltip id={`Tooltip${index}`}>
+                                                                    <div>
+                                                                        {item.info + ': ' + item.object.name}
+                                                                    </div>
+                                                                </Tooltip>
+                                                            }>
+                                                                <img 
+                                                                className='worker-portrait'
+                                                                src={item.object.avatar}
                                                                 />
+
                                                             </OverlayTrigger>
                                                         )
                                                     } else return null
