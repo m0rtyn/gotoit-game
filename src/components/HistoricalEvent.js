6f6dadd2207226ea0251d7e82ae251e0882eb525
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-08-12 16:37:19 +0300

    HistoricalEvent component provided

---
+++ src/components/HistoricalEvent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HistoricalEvent = (props) => {
+  return <div>
+    <div className="flexbox">
+      <span className="flex-grow">
+        <h2>{props.event.title}</h2>
+      </span>
+      <span>
+        <button className="btn btn-warning" onClick={() => {props.closePopup()}}>Close</button>
+      </span>
+    </div>
+    <div>{`Today ${props.date}, ${props.event.description}`}</div>
+  </div>
+};
+
+export default HistoricalEvent
