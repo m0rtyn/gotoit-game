570c2d0149bcfda053f809cbc747604d4263d130
Naddyson <Naddyson777@gmail.com>
2018-08-31 13:43:32 +0300

    eslint fix

--- src/components/Timeline/OverlayTriggers/DeadLine.js
+++ src/components/Timeline/OverlayTriggers/DeadLine.js
@@ -1,10 +1,10 @@
-import { Component } from 'react';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
-import React from 'react';
-import * as PropTypes from 'prop-types';
-import { Avatar } from '../../Projects/Avatar';
-import _ from 'lodash';
-import ReactTooltip from 'react-tooltip';
+import { Component } from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import React from "react";
+import * as PropTypes from "prop-types";
+import { Avatar } from "../../Projects/Avatar";
+import _ from "lodash";
+import ReactTooltip from "react-tooltip";
 
 export default class DeadLine extends Component {
     static propTypes = {
@@ -14,23 +14,18 @@ export default class DeadLine extends Component {
         name: PropTypes.string
     };
 
-  render() {
-    return (
-      <div>
-        <a data-tip>
-          <div style={{ position: 'relative' }}>
-            <Avatar
-              name={this.props.name}
-              sources={_.toPairs(this.props.avatar)}
-              style={{ position: 'absolute' }}
-              size={20}
-            />
-          </div>
-        </a>
-        <ReactTooltip place="bottom">
-          <div>{this.props.info + ': ' + this.props.name}</div>
-        </ReactTooltip>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <a data-tip>
+                    <div style={{ position: "relative" }}>
+                        <Avatar name={this.props.name} sources={_.toPairs(this.props.avatar)} style={{ position: "absolute" }} size={20} />
+                    </div>
+                </a>
+                <ReactTooltip place="bottom">
+                    <div>{this.props.info + ": " + this.props.name}</div>
+                </ReactTooltip>
+            </div>
+        );
+    }
 }
