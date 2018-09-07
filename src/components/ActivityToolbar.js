85c7149cc60022a616e944333edb3b3b393e4cbc
Volodya Martynson <zogacc@gmail.com>
2018-09-07 11:26:56 +0700

    fix tabs for activity column

--- src/components/ActivityToolbar.js
+++ src/components/ActivityToolbar.js
@@ -21,101 +21,104 @@ class ActivityToolbar extends Component {
             return count;
         })();
         return (
-            <ul className="nav nav-tabs nav-tabs-light-mode activity-toolbar">
-                <li className="nav-item">
-                    <a
-                        className="nav-link"
-                        onClick={() => {
-                            data.helpers.changeContent("PublicRelations");
-                        }}
-                    >
-                        Public Relations
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className="nav-link"
-                        onClick={() => {
-                            data.helpers.changeContent("Office");
-                        }}
-                    >
-                        Office
-                    </a>
-                </li>
-                {/*<li className="nav-item">
-          {data.projects.length > 0 ? (
-            <a
-              className="nav-link"
-              onClick={() => {
-                data.helpers.changeContent('StartMeeting');
-              }}
-            >
-              Start Meeting
-            </a>
-          ) : (
-            ''
-          )}
-        </li>*/}
-                <li className="nav-item">
-                    {data.projects_archive_reports.length > 0 ? (
+            <div className="card-header">
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
                         <a
                             className="nav-link"
                             onClick={() => {
-                                data.helpers.changeContent("Archive");
+                                data.helpers.changeContent("Mail");
                             }}
                         >
-                            Archive
+                            <span>Mail </span>
+                            {unread_messages_count !== 0 ? (
+                                <span className="badge badge-pill badge-info">{unread_messages_count}</span>
+                            ) : (
+                                ""
+                            )}
                         </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            className="nav-link"
+                            onClick={() => {
+                                data.helpers.changeContent("PublicRelations");
+                            }}
+                        >
+                            Relations
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            className="nav-link"
+                            onClick={() => {
+                                data.helpers.changeContent("Office");
+                            }}
+                        >
+                            Office
+                        </a>
+                    </li>
+                    {/*<li className="nav-item">
+                        {data.projects.length > 0 ? (
+                            <a
+                            className="nav-link"
+                            onClick={() => {
+                                data.helpers.changeContent('StartMeeting');
+                            }}
+                            >
+                            Start Meeting
+                            </a>
+                        ) : (
+                            ''
+                        )}
+                    </li>*/}
+
+                    <li className="nav-item">
+                        <a
+                            className="nav-link"
+                            onClick={() => {
+                                data.helpers.changeContent("MarketTop");
+                            }}
+                        >
+                            Analitics
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            className="nav-link"
+                            onClick={() => {
+                                data.helpers.changeContent("Loans");
+                            }}
+                        >
+                            Loans
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a
+                            className="nav-link"
+                            onClick={() => {
+                                data.helpers.changeContent("Exchange");
+                            }}
+                        >
+                            Exchange
+                        </a>
+                    </li>
+                    {data.projects_archive_reports.length > 0 ? (
+                        <li className="nav-item">
+                            <a
+                                className="nav-link"
+                                onClick={() => {
+                                    data.helpers.changeContent("Archive");
+                                }}
+                            >
+                                Archive
+                            </a>
+                        </li>
                     ) : (
                         ""
                     )}
-                </li>
-                <li className="nav-item">
-                    <a
-                        className="nav-link"
-                        onClick={() => {
-                            data.helpers.changeContent("MarketTop");
-                        }}
-                    >
-                        Market Analysis
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className="nav-link"
-                        onClick={() => {
-                            data.helpers.changeContent("Loans");
-                        }}
-                    >
-                        Loans
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className="nav-link"
-                        onClick={() => {
-                            data.helpers.changeContent("Exchange");
-                        }}
-                    >
-                        Exchange
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a
-                        className="nav-link"
-                        onClick={() => {
-                            data.helpers.changeContent("Mail");
-                        }}
-                    >
-                        <span>
-                            <i className="fa fa-envelope" />
-                            &nbsp;
-                        </span>
-                        <span>Mail&nbsp;</span>
-                        {unread_messages_count !== 0 ? <span className="badge badge-pill badge-info">{unread_messages_count}</span> : ""}
-                    </a>
-                </li>
-            </ul>
+                </ul>
+            </div>
         );
     }
 }
